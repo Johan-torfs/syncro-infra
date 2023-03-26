d4c0daee38815a648f3d160b00683c69db6c838e
Johan Torfs <johan@pajamallama.be>
2023-03-26 19:24:28 +0200

    more sql queries

---
+++ create-tickets-jan.sql
@@ -0,0 +1,117 @@
+-- Creating 10 ticketss updated/resolved/created 2023-01-25
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 1', '2023-01-25 09:00:00', '2023-01-25 11:00:00', 'Open', 4, 1);
+UPDATE tickets SET resolved_date = '2023-01-25 12:00:00', status = 'Resolved' WHERE id = 1;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 2', '2023-01-25 10:00:00', '2023-01-25 11:30:00', 'Open', 4, 2);
+UPDATE tickets SET resolved_date = '2023-01-25 12:30:00', status = 'Resolved' WHERE id = 2;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket created today', '2023-01-25 12:00:00', '2023-01-25 12:00:00', 'Open', 4, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today', '2023-01-25 13:00:00', '2023-01-25 13:00:00', '2023-01-25 15:00:00', 'Resolved', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today 2', '2023-01-25 14:00:00', '2023-01-25 14:00:00', '2023-01-25 16:00:00', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 3', '2023-01-25 15:00:00', '2023-01-25 17:00:00', 'Open', 5, 3);
+UPDATE tickets SET resolved_date = '2023-01-25 12:00:00', status = 'Resolved' WHERE id = 6;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 4', '2023-01-25 16:00:00', '2023-01-25 17:30:00', 'Open', 5, 1);
+UPDATE tickets SET resolved_date = '2023-01-25 13:00:00', status = 'Resolved' WHERE id = 7;
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today 3', '2023-01-25 17:00:00', '2023-01-25 17:00:00', '2023-01-25 18:00:00', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 101', '2023-01-25 10:02:34', '2023-01-25 10:02:34', 'Open', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 102 (Resolved)', '2023-01-25 11:18:42', '2023-01-25 11:18:42', '2023-01-25 13:45:22', 'Resolved', 4, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 103', '2023-01-25 12:09:55', '2023-01-25 12:09:55', 'Open', 5, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 104 (Resolved)', '2023-01-25 13:33:07', '2023-01-25 15:40:18', '2023-01-25 16:10:02', 'Resolved', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 105', '2023-01-25 14:22:19', '2023-01-25 14:22:19', 'Open', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 106 (Resolved)', '2023-01-25 15:45:21', '2023-01-25 16:18:33', '2023-01-25 17:23:55', 'Resolved', 5, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 107', '2023-01-25 16:55:40', '2023-01-25 16:55:40', 'Open', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 108 (Resolved)', '2023-01-25 17:42:01', '2023-01-25 19:10:10', '2023-01-25 20:25:40', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 109', '2023-01-25 18:29:14', '2023-01-25 18:29:14', 'Open', 4, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 110', '2023-01-25 19:15:57', '2023-01-25 19:15:57', 'Open', 5, 1);
+
+-- Creating 10 ticketss updated/resolved/created 2023-01-26
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 1', '2023-01-26 09:00:00', '2023-01-26 11:00:00', 'Open', 4, 1);
+UPDATE tickets SET resolved_date = '2023-01-26 12:00:00', status = 'Resolved' WHERE id = 1;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 2', '2023-01-26 10:00:00', '2023-01-26 11:30:00', 'Open', 4, 2);
+UPDATE tickets SET resolved_date = '2023-01-26 12:30:00', status = 'Resolved' WHERE id = 2;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket created today', '2023-01-26 12:00:00', '2023-01-26 12:00:00', 'Open', 4, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today', '2023-01-26 13:00:00', '2023-01-26 13:00:00', '2023-01-26 15:00:00', 'Resolved', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today 2', '2023-01-26 14:00:00', '2023-01-26 14:00:00', '2023-01-26 16:00:00', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 3', '2023-01-26 15:00:00', '2023-01-26 17:00:00', 'Open', 5, 3);
+UPDATE tickets SET resolved_date = '2023-01-26 12:00:00', status = 'Resolved' WHERE id = 6;
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket updated today 4', '2023-01-26 16:00:00', '2023-01-26 17:30:00', 'Open', 5, 1);
+UPDATE tickets SET resolved_date = '2023-01-26 13:00:00', status = 'Resolved' WHERE id = 7;
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket resolved today 3', '2023-01-26 17:00:00', '2023-01-26 17:00:00', '2023-01-26 18:00:00', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 101', '2023-01-26 10:02:34', '2023-01-26 10:02:34', 'Open', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 102 (Resolved)', '2023-01-26 11:18:42', '2023-01-26 11:18:42', '2023-01-26 13:45:22', 'Resolved', 4, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 103', '2023-01-26 12:09:55', '2023-01-26 12:09:55', 'Open', 5, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 104 (Resolved)', '2023-01-26 13:33:07', '2023-01-26 15:40:18', '2023-01-26 16:10:02', 'Resolved', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 105', '2023-01-26 14:22:19', '2023-01-26 14:22:19', 'Open', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 106 (Resolved)', '2023-01-26 15:45:21', '2023-01-26 16:18:33', '2023-01-26 17:23:55', 'Resolved', 5, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 107', '2023-01-26 16:55:40', '2023-01-26 16:55:40', 'Open', 4, 1);
+
+INSERT INTO tickets (subject, created_at, updated_at, resolved_date, status, customerId, priorityId) 
+VALUES ('Ticket 108 (Resolved)', '2023-01-26 17:42:01', '2023-01-26 19:10:10', '2023-01-26 20:25:40', 'Resolved', 5, 2);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 109', '2023-01-26 18:29:14', '2023-01-26 18:29:14', 'Open', 4, 3);
+
+INSERT INTO tickets (subject, created_at, updated_at, status, customerId, priorityId) 
+VALUES ('Ticket 110', '2023-01-26 19:15:57', '2023-01-26 19:15:57', 'Open', 5, 1);
